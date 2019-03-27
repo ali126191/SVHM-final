fba41936bf2e9787b4bb853654fb7b6bd94f27e0
ali126191 <ali126191@gmail.com>
2019-03-27 14:57:31 -0600

    Updates

--- sw.js
+++ sw.js
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "cbecac9831f0c6adb49198b0a9ef93ef"
+    "revision": "9b056fc5b67a3d853cbfcdd2ea544383"
   },
   {
     "url": "static/d/604/path---offline-plugin-app-shell-fallback-a-30-c5a-BawJvyh36KKFwbrWPg4a4aYuc8.json"
